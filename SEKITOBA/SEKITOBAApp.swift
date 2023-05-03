a1ff78e7015d232475483a56af4d8b956e0e42fa
xxminamixx <pencil3215@gmail.com>
2023-05-04 07:55:40 +0900

    Initial Commit

---
+++ SEKITOBA/SEKITOBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEKITOBAApp.swift
+//  SEKITOBA
+//
+//  Created by 南京兵 on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct SEKITOBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
